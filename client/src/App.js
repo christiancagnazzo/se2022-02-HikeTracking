6700b3a39818fc6164803238892c11c7b24e1818
GioPom <s304868@studenti.polito.it>
2022-11-06 01:01:33 +0100

    starting project, API for registering, navbar ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import './custom.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import API from './API';
+import Visitor from './components/visitor';
+import { Container, Row } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { LoginForm } from './components/login';
+import MyNavbar2 from './components/navbarlogin';
 
-function App() {
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <App2/>
+    </Router>
+  )
+}
+function App2() {
+  const [loggedIn,setLoggedIn]=useState(false);
+  const [user, setUser] = useState({});
+  //const [flag,setFlag]=useState(false)
+  const [message, setMessage] = useState('');
+  //const [services,setServices]=useState([1]);
+  //const [dirty,setDirty]=useState(true);
+  //const [ticket,setTicket]=useState('');
+  const [userPower,setUserPower]=useState("Visitor")
+
+  function handleError(err){
+    console.log(err);
+  }
+  const navigate = useNavigate();
+  
+  
+    const doLogout = async () => {
+      await API.logout();
+      setLoggedIn(false);
+      setUser({});
+      setUserPower("Visitor")
+      navigate('/Visitor');
+    }
+  
+    // inserisco come nome l'email della persona e lo userPower sono i privilegi disponibili
+    const login = ()=>{navigate("/login")}
+    const doLogin = (credentials) => {
+      API.login(credentials)
+        .then( user => {
+          setLoggedIn(true);
+          setUser(user.mail);
+          setUserPower(user.power)
+          //setDirty(true);
+          setMessage('');
+          navigate('/'+user.power);
+        })
+        .catch(err => {
+          setMessage(err);
+        }
+          )
+    }
+
+
+  return (
+    <>
+    <MyNavbar2 loggedIn={loggedIn} logout={doLogout} login={login} userPower={userPower}/>
+    <Container fluid>
+       <Row className="vheight-100">
+            <Routes> 
+               <Route path='/Visitor' element={(loggedIn ? <Navigate to='/'userPower /> : <Visitor ></Visitor>)}></Route>
+               <Route path='/login'  element={loggedIn ? <Navigate to='/'userPower /> : <LoginForm login={doLogin} loginError={message} setLoginError={setMessage} /> }/>
+            </Routes>
+       </Row>
+    </Container>
+    </>
+    );
 }
 
 export default App;
---
+++ client/src/components/visitor.js
@@ -0,0 +1,8 @@
+function Visitor(props) {
+
+    return (       
+        <h1 className="col-12 below-nav">Hey! non riceviamo alcuna risposta dal server! Prova a riaggiornare la pagina!</h1>
+        
+    );
+}
+export default Visitor;
