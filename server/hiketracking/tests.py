ec26542f0423fc8e20ebdbcb5be07a96b807976b
kangchenghua <chenghuakang@gmail.com>
2022-11-12 06:55:03 +0100

    Update tests.py
    add login test and logout test

--- server/hiketracking/tests.py
+++ server/hiketracking/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.test import TestCase
+from django.http import request
 
 
 class UsersManagersTests(TestCase):
@@ -43,3 +44,54 @@ class UsersManagersTests(TestCase):
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
                 email='super@user.com', password='foo',role="hi" ,is_superuser=False)
+
+# try to login and the check result of login
+# password is incurrect and login failed
+# currect information and login success
+# because of this project uses njango default login, it test njango api using in fact
+    def test_login(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='john',email= 'normal@user.com',password= 'johnpassword')
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        username = request.POST['john']
+        password = request.POST['johnpassword']
+        user = authenticate(request, username=username, password=password)
+        self.assertIsNotNone(user)
+        with assertRaises(ValueError):
+            login(request, user)
+
+        username = request.POST['john']
+        password = request.POST['normal@user.com']
+        login(request, user)
+        self.assertTrue(request.user.is_authenticated)
+
+# try to logout and the check result of logout
+    def test_logout(self):
+        User = get_user_model()
+        user = User.objects.create_user('john', 'normal@user.com', 'johnpassword')
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        username = request.POST['john']
+        password = request.POST['normal@user.com']
+        user = authenticate(request, username=username, password=password)
+        self.assertIsNotNone(user)
+        login(request, user)
+        self.assertTrue(request.user.is_authenticated)
+        logout(request)
+        self.assertFalse(request.user.is_authenticated)
+
+
+
+
+            
+
+
+
+
