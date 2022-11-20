1672af3a72142d81799268ea3227e75a9555a9b0
kangchenghua <chenghuakang@gmail.com>
2022-11-20 23:50:49 +0100

    Update localguide.js

--- client/src/components/localguide.js
+++ client/src/components/localguide.js
@@ -64,21 +64,19 @@ function LocalGuide(props) {
     
     console.log(e.target.className);
     var objFile = document.getElementById("formFile").files[0];
-
-    setErrorMessage(objFile);
-    //var files = inp.prop('files');
     if (objFile.length == 0) {
     } else {
       var reader = new FileReader();  
       reader.onload = function (evt) {
         var fileString = this.result;
-        setErrorMessage(fileString)
+        //setErrorMessage(fileString)
         gpx.parse(fileString);
         let track1 = gpx.tracks[0];
         let point1 = track1.points[0];
-        let endPoint = track1.points[-1];
+        let endPoint = track1.points[1];
         setSp([point1.lat,point1.lon]);
         setEp([endPoint.lat,endPoint.lon]);
+        
     }
     reader.readAsText(objFile, "UTF-8");
   }
