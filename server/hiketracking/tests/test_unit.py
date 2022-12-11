46b6e462d05391871a865550780e0935f887c808
kangchenghua <chenghuakang@gmail.com>
2022-12-11 09:16:56 +0100

    Update test_unit.py

--- server/hiketracking/tests/test_unit.py
+++ server/hiketracking/tests/test_unit.py
@@ -107,7 +107,45 @@ class modifyAndDeleteHikeUnitTest(TestCase):
         c.login(username="test@user.com",password="foo")
         response = c.get('/hiketracking/hikes/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content,b'''[{"id":1,"title":"Climbing","length":2,"expected_time":1,"ascent":1,"difficulty":"easy","description":"","track_file":"","start_point_id":1,"end_point_id":1,"local_guide_id":1,"condition":"Open","condition_description":" ","rp":[],"start_point_lat":0.01,"start_point_lng":0.01,"start_point_address":"test address","end_point_lat":0.01,"end_point_lng":0.01,"end_point_address":"test address"},{"id":2,"title":"Trekking","length":3,"expected_time":2,"ascent":0,"difficulty":"medium","description":"","track_file":"","start_point_id":1,"end_point_id":1,"local_guide_id":1,"condition":"Open","condition_description":" ","rp":[],"start_point_lat":0.01,"start_point_lng":0.01,"start_point_address":"test address","end_point_lat":0.01,"end_point_lng":0.01,"end_point_address":"test address"}]''')
+        self.assertEqual(response.content,b'[{"id":1,'
+        b'"title":"Climbing",'
+        b'"length":2,"expected_time":1,'
+        b'"ascent":1,'
+        b'"difficulty":"easy",'
+        b'"description":"",'
+        b'"track_file":"",'
+        b'"start_point_id":1,'
+        b'"end_point_id":1,'
+        b'"local_guide_id":1,'
+        b'"condition":"Open",'
+        b'"condition_description":" ",'
+        b'"rp":[],'
+        b'"start_point_lat":0.01,'
+        b'"start_point_lng":0.01,'
+        b'"start_point_address":"test address",'
+        b'"end_point_lat":0.01,'
+        b'"end_point_lng":0.01,'
+        b'"end_point_address":"test address"},'
+        b'{"id":2,'
+        b'"title":"Trekking",'
+        b'"length":3,'
+        b'"expected_time":2,'
+        b'"ascent":0,'
+        b'"difficulty":"medium",'
+        b'"description":"",'
+        b'"track_file":"",'
+        b'"start_point_id":1,'
+        b'"end_point_id":1,'
+        b'"local_guide_id":1,'
+        b'"condition":"Open",'
+        b'"condition_description":" ",'
+        b'"rp":[],'
+        b'"start_point_lat":0.01,'
+        b'"start_point_lng":0.01,'
+        b'"start_point_address":"test address",'
+        b'"end_point_lat":0.01,'
+        b'"end_point_lng":0.01,'
+        b'"end_point_address":"test address"}]')
     
     @mock.patch("django.test.Client.post",return_value=MockHike())
     def test_modifyHike(self,mocked):
@@ -122,6 +160,10 @@ class modifyAndDeleteHikeUnitTest(TestCase):
         c.delete('/hiketracking/hikes/')
         hike2 = Hike.objects.all()
         self.assertTrue(hike2.exists())
+
+
+
+
 
 
 
