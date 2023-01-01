9de96ddf5cf3ea04f3fa1a1c47a9a3e46edc906b
kangchenghua <chenghuakang@gmail.com>
2023-01-01 15:10:41 +0100

    Update test_unit.py

--- server/hiketracking/tests/test_unit.py
+++ server/hiketracking/tests/test_unit.py
@@ -15,8 +15,9 @@ RESPONSE_UTIL = b'[{"id":1,'\
         b'"start_point_id":1,'\
         b'"end_point_id":1,'\
         b'"local_guide_id":1,'\
+        b'"picture":"./hikePictures/defultImage.jpg",'\
         b'"condition":"Open",'\
-        b'"condition_description":" ",'\
+        b'"condition_description":"Open",'\
         b'"rp":[],'\
         b'"start_point_lat":0.01,'\
         b'"start_point_lng":0.01,'\
@@ -35,8 +36,9 @@ RESPONSE_UTIL = b'[{"id":1,'\
         b'"start_point_id":1,'\
         b'"end_point_id":1,'\
         b'"local_guide_id":1,'\
+        b'"picture":"./hikePictures/defultImage.jpg",' \
         b'"condition":"Open",'\
-        b'"condition_description":" ",'\
+        b'"condition_description":"Open",'\
         b'"rp":[],'\
         b'"start_point_lat":0.01,'\
         b'"start_point_lng":0.01,'\
@@ -44,6 +46,8 @@ RESPONSE_UTIL = b'[{"id":1,'\
         b'"end_point_lat":0.01,'\
         b'"end_point_lng":0.01,'\
         b'"end_point_address":"test address"}]'
+
+BACK_FROUND=1
 
 MOCK_UTIL = {
             "user":
@@ -77,7 +81,14 @@ class UserProfileUnitTest(TestCase):
     )
         c1 = CustomUser(email = "test@test.com",role = "Testrole")
         c1.save()
-        CustomerProfile.objects.create(user =c1,min_length = 0.01,max_length = 0.01,min_time = 1,max_time = 1,min_altitude = 1,max_altitude = 1)
+        CustomerProfile.objects.create(user =c1,
+        min_length = 0.01,
+        max_length = 0.01,
+        min_time = 1,
+        max_time = 1,
+        min_altitude = 1,
+        max_altitude = 1)
+
         self.url='/hiketracking/profile/'
         return super().setUp()
    
@@ -150,10 +161,29 @@ def set_up():
     User = get_user_model()
     User.objects.create_user(email='test@user.com', password='foo', role='smth')
     user_id = User.objects.get(email='test@user.com')
-    p1 = Point(latitude=0.01, longitude=0.01, province="test province", village="test village",address="test address")
+
+    p1 = Point(latitude=0.01, longitude=0.01, province="test province", 
+    village="test village",
+    address="test address")
+
     p1.save()
-    Hike.objects.create(title='Climbing', length=2, expected_time=1, ascent=1,difficulty='easy',start_point=p1,end_point=p1,local_guide=user_id)
-    Hike.objects.create(title='Trekking', length=3, expected_time=2, ascent=0,difficulty='medium',start_point=p1,end_point=p1,local_guide=user_id) 
+
+    Hike.objects.create(title='Climbing', 
+    length=2, 
+    expected_time=1, 
+    ascent=1,
+    difficulty='easy',
+    start_point=p1,
+    end_point=p1,
+    local_guide=user_id)
+    Hike.objects.create(title='Trekking', 
+    length=3, 
+    expected_time=2, 
+    ascent=0,
+    difficulty='medium',
+    start_point=p1,
+    end_point=p1,
+    local_guide=user_id) 
     
  
 class modifyAHikeUnitTest(TestCase):
@@ -175,10 +205,30 @@ class modifyAndDeleteHikeUnitTest(TestCase):
         User = get_user_model()
         User.objects.create_user(email='test@user.com', password='foo', role='smth')
         user_id = User.objects.get(email='test@user.com')
-        p1 = Point(latitude=0.01, longitude=0.01, province="test province", village="test village",address="test address")
+        p1 = Point(latitude=0.01, longitude=0.01, 
+        province="test province", 
+        village="test village",
+        address="test address")
+
         p1.save()
-        Hike.objects.create(title='Climbing', length=2, expected_time=1, ascent=1,difficulty='easy',start_point=p1,end_point=p1,local_guide=user_id)
-        Hike.objects.create(title='Trekking', length=3, expected_time=2, ascent=0,difficulty='medium',start_point=p1,end_point=p1,local_guide=user_id) 
+        Hike.objects.create(title='Climbing', 
+        length=2, 
+        expected_time=1, 
+        ascent=1,
+        difficulty='easy',
+        start_point=p1,
+        end_point=p1,
+        local_guide=user_id)
+
+        Hike.objects.create(title='Trekking', 
+        length=3, 
+        expected_time=2, 
+        ascent=0,
+        difficulty='medium',
+        start_point=p1,
+        end_point=p1,
+        local_guide=user_id) 
+
         return super().setUp()
     
     def test_Backfround(self):
@@ -208,8 +258,6 @@ class DeleteHikeUnitTest(TestCase):
         response = c.get('/hiketracking/hikes/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, RESPONSE_UTIL)
-    
-        self.assertEqual(response.content,b'''[{"id":1,"title":"Climbing","length":2,"expected_time":1,"ascent":1,"difficulty":"easy","description":"","track_file":"","start_point_id":1,"end_point_id":1,"local_guide_id":1,"condition":"Open","condition_description":" ","rp":[],"start_point_lat":0.01,"start_point_lng":0.01,"start_point_address":"test address","end_point_lat":0.01,"end_point_lng":0.01,"end_point_address":"test address"},{"id":2,"title":"Trekking","length":3,"expected_time":2,"ascent":0,"difficulty":"medium","description":"","track_file":"","start_point_id":1,"end_point_id":1,"local_guide_id":1,"condition":"Open","condition_description":" ","rp":[],"start_point_lat":0.01,"start_point_lng":0.01,"start_point_address":"test address","end_point_lat":0.01,"end_point_lng":0.01,"end_point_address":"test address"}]''')
         
     def test_modifyHike(self):
         c = Client()
@@ -223,4 +271,78 @@ class DeleteHikeUnitTest(TestCase):
         c.delete('/hiketracking/hikes/')
         hike2 = Hike.objects.all()
         self.assertTrue(hike2.exists())
+
+
+
+
+class MockRecordPoint:
+    def __init__(self):
+        self.status_code = 200
+ 
+    def json(self):
+        return {
+            'id': 1,
+            'title':'test_title',
+            'length': 1,
+            'expected_time': 1,
+            'ascent':  1,
+            'difficulty': 'hard',
+            'description': "test description",
+            'track_file': "test file",
+        }
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
+
+
+class MockStats:
+    def __init__(self):
+        self.status_code = 200
+ 
+    def json(self):
+        return {
+            'id': 1,
+            'title':'test_title',
+            'length': 1,
+            'expected_time': 1,
+            'ascent':  1,
+            'difficulty': 'hard',
+            'description': "test description",
+            'track_file': "test file",
+        }
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
+class PerformanceStatsUnitTest(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+class RecordPointUnitTest(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+class MockWeatherAlert:
+    def __init__(self):
+        self.status_code = 200
+ 
+    def json(self):
+        return {
+            'id': 1,
+            'title':'test_title',
+            'length': 1,
+            'expected_time': 1,
+            'ascent':  1,
+            'difficulty': 'hard',
+            'description': "test description",
+            'track_file': "test file",
+        }
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
+class WeatherAlertUnitTest(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
 
