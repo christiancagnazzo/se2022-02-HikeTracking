a72c46368986337a72f63e4cd0fe3682b0012fb2
Jaco <jacopo.decristofaro@external.eni.com>
2023-01-06 16:33:08 +0100

    fix bugs

--- client/src/API.js
+++ client/src/API.js
@@ -289,7 +289,7 @@ function _arrayBufferToBase64(buffer) {
   let binary = '';
   const bytes = new Uint8Array(buffer);
   const len = bytes.byteLength;
-  for (const i = 0; i < len; i++) {
+  for (let i = 0; i < len; i++) {
     binary += String.fromCharCode(bytes[i]);
   }
   return window.btoa(binary);
